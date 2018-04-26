ec427dcc4e7a3b70d5fe1586fcb841d41999f74f
Michael <metamike14@gmail.com>
2018-04-25 17:26:24 -0700

    Add function concatenate

--- re2nfa.py
+++ re2nfa.py
@@ -1,8 +1,9 @@
-class NFA:
-    start = 0
-    accept = 0
-    deltaArray = []
-    EArray = []
+class NFA(object):
+    def __init__(self):
+        self.start = 0
+        self.accept = 0
+        self.deltaArray = []
+        self.EArray = []
 
     def baseNFA(self, char, m):
         self.accept = 1
@@ -22,7 +23,28 @@ class NFA:
         return
 
     def concatenate(self, otherNFA):
-        return
+        newNFA = NFA()
+        A = len(self.deltaArray)
+        B = len(otherNFA.deltaArray)
+        for i in range(A):
+            newNFA.deltaArray.append(self.deltaArray[i])
+            if i < A - 1:
+                newNFA.EArray.append(self.EArray[i])
+            else:
+                newNFA.EArray.append(self.EArray[i])
+                newNFA.EArray[i].append(A)
+        for i in range(B):
+            temp = []
+            for k in range(len(otherNFA.deltaArray[i])):
+                if otherNFA.deltaArray[i][k] is not None:
+                    temp.append(otherNFA.deltaArray[i][k]+A)
+                else:
+                    temp.append(None)
+            newNFA.deltaArray.append(temp)
+            newNFA.EArray.append(otherNFA.EArray[i])
+        self.deltaArray = newNFA.deltaArray
+        self.EArray = newNFA.EArray
+        self.accept = A+B
 
     def star(self):
         return
@@ -33,6 +55,10 @@ def main():
     m = [['0', 0], ['1', 1]]
     nfa1 = NFA()
     nfa1.baseNFA('1', m)
+
+    nfa2 = NFA()
+    nfa2.baseNFA('0', m)
+    nfa1.concatenate(nfa2)
 
     return 0
 
